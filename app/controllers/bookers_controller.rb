73ad01fe820eaff96729e7efd6548a5f9f85cdaf
tako0820 <105572635+tako0820@users.noreply.github.com>
2022-06-10 05:48:47 +0000

    フラッシュメッセージまで

--- app/controllers/bookers_controller.rb
+++ app/controllers/bookers_controller.rb
@@ -1,2 +1,46 @@
 class BookersController < ApplicationController
+
+
+def index
+ @books = Book.all
+end
+
+def show
+ @book = Book.find(params[:id])
+end
+
+def edit
+ @book = Book.find(params[:id])
+end
+
+def create
+ @book = Book.new(book_params)
+ if@book.save
+ redirect_to book_path(@book.id)
+ flash[:notice] = "Book was successfully created."
+ else
+ render :index
+ end
+end
+
+def update
+ book = Book.find(params[:id])
+ book.update(book_params)
+ redirect_to book_path(book.id)
+ flash[:notice] = "Book was successfully updated."
+end
+
+def destroy
+  book = Book.find(params[:id])
+  book.destroy
+  redirect_to '/books'
+end
+
+private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 get '/top' => 'homes#top'
-get 'edit' => 'bookers#edit'
-get 'books' => 'bookers#index'
-get 'show' => 'bookers#show'
+get '/books' => 'bookers#index'
+get '/books/:id' => 'bookers#show',as: 'book'
+post '/books' => 'bookers#create'
+get '/books/:id/edit' => 'bookers#edit', as: 'edit_book'
+patch '/books/:id' => 'bookers#update', as: 'update_book'
+delete 'books/:id' => 'bookers#destroy', as: 'destroy_book'
+
 
 end
